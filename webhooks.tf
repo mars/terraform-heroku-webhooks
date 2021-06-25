7e1dedf2ea129cf2e39155eae53b34c14fbf2ceb
Mars Hall <mars@users.noreply.github.com>
2021-06-25 14:23:42 -0700

    Fixup HCL syntax

--- webhooks.tf
+++ webhooks.tf
@@ -1,5 +1,5 @@
 resource "heroku_app_webhook" "example_release" {
-  app_id  = "${heroku_app.example.id}"
+  app_id  = heroku_app.example.id
   level   = "sync"
   url     = local.reqinspeq_app_url
   include = ["api:release"]
